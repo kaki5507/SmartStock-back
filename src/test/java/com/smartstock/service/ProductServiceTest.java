978dfebcd2f36277b0fc63fb338e34b132572b38
kaki5507 <ckdbioksh@naver.com>
2024-09-27 11:10:20 +0900

    test(테스트 케이스 수정)  - 테스트 케이스 작성  - asssertNotNull 수정해야함

---
+++ src/test/java/com/smartstock/service/ProductServiceTest.java
@@ -0,0 +1,35 @@
+package com.smartstock.service;
+
+import com.smartstock.model.Product;
+import com.smartstock.SmartStockBackendApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest(classes = SmartStockBackendApplication.class)
+public class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void testCreateProduct() {
+        Product product = new Product();
+        product.setName("Test Product");
+        product.setPrice(19.99);
+        product.setQuantity(10);
+
+        Product createdProduct = productService.createProduct(product);
+
+        assertNotNull(createdProduct);
+        assertEquals("Test Product", createdProduct.getName());
+    }
+
+    @Test
+    public void testGetAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        assertNotNull(products);
+    }
+}
